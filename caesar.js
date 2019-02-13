1fbb1507dcd442bdc0d19cb23cb48d18d1858606
Michael Chilaka <chilakamichael001@gmail.com>
2019-02-13 11:44:09 +0100

    caesar.js

--- caesar.js
+++ caesar.js
@@ -1,13 +1,67 @@
-function caesarCipher(str, num){
-    let lowerCaseString = str.toLowerCase();
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
-    let newString = '';
+"use strict";
 
-    for (let i = 0; i < lowerCaseString.length; i++){
-        let currentLetter = lowerCaseString[i];
-        if (currentLetter = ' '){
-            newString += currentLetter;
-            continue;
-        }
+// function caesarCipher(str, num){
+//     num = num % 26;
+//     let lowerCaseString = str.toLowerCase();
+//     let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+//     let newString = '';
+
+//     for (let i = 0; i < lowerCaseString.length; i++){
+//         let currentLetter = lowerCaseString[i];
+//         if (currentLetter = ' '){
+//             newString += currentLetter;
+//             continue;
+//         }
+//         let currentIndex = alphabet.indexOf(currentLetter);
+//         let newIndex = currentIndex + num;
+//         if (newIndex > 25) newIndex = newIndex - 26;
+//         if (newIndex < 0) newIndex = 26 + newIndex;
+//         if (str[i] === str[i].toUpperCase()) {
+//             newString += alphabet[newIndex].toUpperCase;
+//         } else {
+//             newString += alphabet[newIndex]
+//         }
+//     }
+//     return newString;
+// }
+
+// let result = caesarCipher('Zoo Keeper', 2);
+
+// console.log(result);
+// caesarCipher('Big Car', -16);
+
+// caesarCipher('JavaScript', -900);
+
+function caesarCipher(str, num) {
+    num = num % 26;
+    var lowerCaseString = str.toLowerCase();
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    var newString = ""
+  
+    for(var i = 0; i < lowerCaseString.length; i++){
+      var currentLetter = lowerCaseString[i];
+      if (currentLetter === " "){
+        newString += currentLetter;
+        continue;
+      }
+      var currentIndex = alphabet.indexOf(currentLetter);
+      var newIndex = currentIndex + num;
+      if(newIndex > 25){
+        newIndex -= 26;
+      }
+      if(newIndex < 0) {
+        newIndex += 26;
+      }
+      if (str[i] === str[i].toUpperCase()){
+        newString += alphabet[newIndex].toUpperCase();
+      } else {
+        newString += alphabet[newIndex];
+      }
     }
-}
+    return newString;
+  }
+  
+  let result = caesarCipher("Julius Caesar", -1)
+   console.log(result)
+
+
