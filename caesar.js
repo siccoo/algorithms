23eb76df133de108236a517c64f6db97ea216d00
Michael Chilaka <chilakamichael001@gmail.com>
2019-02-13 13:19:24 +0100

    Final changes to caesar.js

--- caesar.js
+++ caesar.js
@@ -29,43 +29,41 @@ function caesarCipher(str, num){
     return newString;
 }
 
-let result = caesarCipher("Zoo Keeper", -2);
+let result = caesarCipher("Matilda Girl", 6);
 
 console.log(result);
-// caesarCipher('Big Car', -16);
 
-// caesarCipher('JavaScript', -900);
 
-// function caesarCipher(str, num) {
-//     num = num % 26;
-//     let lowerCaseString = str.toLowerCase();
-//     let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-//     let newString = ""
+function caesarCipher(str, num) {
+    num = num % 26;
+    let lowerCaseString = str.toLowerCase();
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+    let newString = ""
   
-//     for(let i = 0; i < lowerCaseString.length; i++){
-//       let currentLetter = lowerCaseString[i];
-//       if (currentLetter === " "){
-//         newString += currentLetter;
-//         continue;
-//       }
-//       let currentIndex = alphabet.indexOf(currentLetter);
-//       let newIndex = currentIndex + num;
-//       if(newIndex > 25){
-//         newIndex -= 26;
-//       }
-//       if(newIndex < 0) {
-//         newIndex += 26;
-//       }
-//       if (str[i] === str[i].toUpperCase()){
-//         newString += alphabet[newIndex].toUpperCase();
-//       } else {
-//         newString += alphabet[newIndex];
-//       }
-//     }
-//     return newString;
-//   }
+    for(let i = 0; i < lowerCaseString.length; i++){
+      let currentLetter = lowerCaseString[i];
+      if (currentLetter === " "){
+        newString += currentLetter;
+        continue;
+      }
+      let currentIndex = alphabet.indexOf(currentLetter);
+      let newIndex = currentIndex + num;
+      if(newIndex > 25){
+        newIndex -= 26;
+      }
+      if(newIndex < 0) {
+        newIndex += 26;
+      }
+      if (str[i] === str[i].toUpperCase()){
+        newString += alphabet[newIndex].toUpperCase();
+      } else {
+        newString += alphabet[newIndex];
+      }
+    }
+    return newString;
+  }
   
-//   let result = caesarCipher("Julius Caesar", -1)
-//    console.log(result)
+  let resulte = caesarCipher("Julius Caesar", -1)
+   console.log(resulte)
 
 
