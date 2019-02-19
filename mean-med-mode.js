6657ed994a68605705b841e034df6264d8f94e07
Michael Chilaka <chilakamichael001@gmail.com>
2019-02-19 11:49:36 +0100

    mean-med-mode.js

---
+++ mean-med-mode.js
@@ -0,0 +1,19 @@
+function meanMedianMode(array) {
+    return {
+        mean: getMean(array),
+        median: getMedian(array),
+        mode: getMode(array)
+    }
+}
+
+function getMean(array) {
+
+}
+
+function getMedian(array) {
+
+}
+
+function getMode(array) {
+
+}
