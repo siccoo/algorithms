643a228cdf818b5a0e7895fb4b2b3ce933f3bb49
Michael Chilaka <chilakamichael001@gmail.com>
2019-02-19 12:51:47 +0100

    mean-med-mode.js changes added

--- mean-med-mode.js
+++ mean-med-mode.js
@@ -7,13 +7,34 @@ function meanMedianMode(array) {
 }
 
 function getMean(array) {
+    let sum = 0;
+    array.forEach(num => {
+        sum += num;
+    });
 
+    let mean = sum / array.length;
+    return mean;
 }
 
 function getMedian(array) {
+    array.sort(function(c, d) {return c - d});
+    let median;
 
+    if (array.length % 2 !== 0) {
+        median = array[Math.floor(array.length / 2)];
+    } else {
+        let mid1 = array[(array.length / 2) - 1];
+        let mid2 = array[array.length / 2];
+        median = (mid1 + mid2) / 2;
+    }
+
+    return median;
 }
 
 function getMode(array) {
+    let modeObj = {};
 
+    array.forEach(num => {
+
+    });
 }
