44926d94f0c401372118c6da88a6e4bd99a47b32
Michael Chilaka <chilakamichael001@gmail.com>
2019-02-25 12:31:45 +0100

    max.js

--- max.js
+++ max.js
@@ -8,16 +8,22 @@ function maxStockProfit(pricesArr) {
     for (let i = 0; i < pricesArr.length; i++) {
         if (changeBuyPrice) {
             buyPrice = pricesArr[i];
-            sellPrice = pricesArr[i + 1];
         }
+            sellPrice = pricesArr[i + 1];
 
         if (sellPrice < buyPrice) {
             changeBuyPrice = true;
         } else {
             let tempProfit = sellPrice - buyPrice;
-            if (tempProfit === maxProfit) {
-                
+            if (tempProfit > maxProfit) {
+                maxProfit = tempProfit;
             }
+                changeBuyPrice = false;
         }
     }
+
+    return maxProfit;
 }
+
+let result = maxStockProfit([ 10, 18, 4, 5, 9, 6, 16, 12 ])
+console.log(result);
